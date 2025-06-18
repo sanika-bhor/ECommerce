2c60a7db72e40f628fecd3c44085aa5ed728222c
sanika-bhor <bhorsanika0239@gmail.com>
2025-06-18 23:22:21 +0530

    dashboard design

--- ECommerceApplication/obj/Debug/net8.0/ECommerceApplication.AssemblyInfo.cs
+++ ECommerceApplication/obj/Debug/net8.0/ECommerceApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b8f2d340d6c629f4d268ddf8eb959c59eb7efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9660c4a7a3fface8c6a84036736dc9273d2e5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
