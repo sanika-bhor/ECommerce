bf1d51dea6e6f582c9dde8db8455dd7b0f3db85e
sanika-bhor <bhorsanika0239@gmail.com>
2025-06-21 18:22:07 +0530

    design contact us page using ai

--- ECommerceApplication/obj/Debug/net8.0/ECommerceApplication.AssemblyInfo.cs
+++ ECommerceApplication/obj/Debug/net8.0/ECommerceApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e24f848f53d08b8b198c408f68535d7082a7a3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064f609735b1d033f7a8aa292a5a550b89b76d67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
